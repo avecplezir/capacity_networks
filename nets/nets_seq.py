9dd4d008ce038d1dfff88888f9e86141559b9841
avecplezir <avecplezir@gmail.com>
2024-03-30 16:46:12 -0400

    attention

--- nets/nets_seq.py
+++ nets/nets_seq.py
@@ -54,7 +54,7 @@ class QNetworkLSTM(nn.Module):
             nn.Linear(512, 512),
         )
 
-        self.lstm = nn.LSTM(512, 128, batch_first=False, num_layers=2)
+        self.lstm = nn.LSTM(512, 128, batch_first=False, num_layers=1)
         self.predictor = nn.Linear(128, env.single_action_space.n)
 
     def forward(self, x, hiddens):
@@ -83,7 +83,7 @@ class QNetworkLSTM(nn.Module):
         # hidden and cell states (see https://youtu.be/8HyCNIVRbSU)
         return {'h': h, 'c': c}
 
-class QNetworkLSTM(nn.Module):
+class QNetworkLSTMNamedTuple(nn.Module):
     def __init__(self, env, args):
         super().__init__()
         self.args = args
@@ -214,3 +214,42 @@ class QNetworkMinAtarLSTM(nn.Module):
             torch.zeros(self.args.num_envs, self.lstm.num_layers, self.lstm.hidden_size).to(self.args.device),
         # hidden and cell states (see https://youtu.be/8HyCNIVRbSU)
         return {'h': h, 'c': c}
+
+
+from attention import AttentionBlock
+
+class QNetworkMinAtarTransformer(nn.Module):
+    def __init__(self, env, args):
+        super().__init__()
+        self.args = args
+        self.encoder = nn.Sequential(
+            nn.Conv2d(args.input_channels, 16, 3, stride=1),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, 2, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, stride=1),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(128, 256),
+        )
+
+        self.attn = AttentionBlock(n_embd=256, n_head=8, block_size=args.seq_len)
+        self.predictor = nn.Linear(256, env.single_action_space.n)
+
+    def forward(self, x, hiddens):
+        if len(x.shape) == 4:
+            x = x.unsqueeze(0)
+        # inverse channels for minatari
+        x = x.permute(0, 1, -1, -3, -2).contiguous()
+        s, b, ch, w, h = x.shape
+        x = x.view(s * b, ch, w, h)
+        out = self.encoder(x / 255.0)
+        out = out.view(s, b, -1)
+        out = out.permute(1, 0, 2).contiguous()
+        out = self.attn(out)
+        out = out.permute(1, 0, 2).contiguous()
+        out = self.predictor(out)
+        return out, {}
+
+    def init_net_hiddens(self):
+        return {}
